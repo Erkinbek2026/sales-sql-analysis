02553c1548dea6917ac1b91e71e92db418aa04f9
Erkinbek2026 <eshombinov@gmail.com>
2026-05-01 10:32:07 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,8 +1,49 @@
+-- 1. Общее количество записей
 SELECT COUNT(*) FROM train;
 
+-- 2. Максимальная продажа
 SELECT MAX(Sales) FROM train;
 
+-- 3. Средний чек
+SELECT AVG(Sales) FROM train;
+
+-- 4. Топ-5 городов по продажам
+SELECT City, SUM(Sales) AS total_sales
+FROM train
+GROUP BY City
+ORDER BY total_sales DESC
+LIMIT 5;
+
+-- 5. Регион с максимальными продажами
+SELECT Region, SUM(Sales) AS total_sales
+FROM train
+GROUP BY Region
+ORDER BY total_sales DESC
+LIMIT 1;
+
+-- 6. Топ-5 клиентов по сумме покупок
 SELECT "Customer Name", SUM(Sales) AS total_spent
 FROM train
 GROUP BY "Customer Name"
-ORDER BY total_spent DESC;
+ORDER BY total_spent DESC
+LIMIT 5;
+
+-- 7. Клиент с максимальной покупкой
+SELECT "Customer Name", Sales
+FROM train
+ORDER BY Sales DESC
+LIMIT 1;
+
+-- 8. Топ категорий по продажам
+SELECT Category, SUM(Sales) AS total_sales
+FROM train
+GROUP BY Category
+ORDER BY total_sales DESC
+LIMIT 3;
+
+-- 9. Самый прибыльный товар
+SELECT "Product Name", SUM(Sales) AS total_sales
+FROM train
+GROUP BY "Product Name"
+ORDER BY total_sales DESC
+LIMIT 1;
